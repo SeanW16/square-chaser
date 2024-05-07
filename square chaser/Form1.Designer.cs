8dca8a839e21000638666411be050f5e9750f91b
seanwood453 <seanwood453@SDS-L-FGHDF63>
2024-05-07 14:46:38 -0400

    Add project files.

---
+++ square chaser/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace square_chaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel1 = new System.Windows.Forms.Label();
+            this.scoreLabel2 = new System.Windows.Forms.Label();
+            this.powerUpTimer = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.powerUpTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // scoreLabel1
+            // 
+            this.scoreLabel1.AutoSize = true;
+            this.scoreLabel1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel1.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel1.Location = new System.Drawing.Point(12, -3);
+            this.scoreLabel1.Name = "scoreLabel1";
+            this.scoreLabel1.Size = new System.Drawing.Size(140, 21);
+            this.scoreLabel1.TabIndex = 0;
+            this.scoreLabel1.Text = "Player 1 Score: 0";
+            // 
+            // scoreLabel2
+            // 
+            this.scoreLabel2.AutoSize = true;
+            this.scoreLabel2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel2.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel2.Location = new System.Drawing.Point(432, -3);
+            this.scoreLabel2.Name = "scoreLabel2";
+            this.scoreLabel2.Size = new System.Drawing.Size(140, 21);
+            this.scoreLabel2.TabIndex = 1;
+            this.scoreLabel2.Text = "Player 2 Score: 0";
+            // 
+            // powerUpTimer
+            // 
+            this.powerUpTimer.Enabled = true;
+            this.powerUpTimer.Interval = 5000;
+            this.powerUpTimer.Tick += new System.EventHandler(this.powerUpTimer_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(224, -3);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 21);
+            this.winLabel.TabIndex = 2;
+            // 
+            // powerUpTimer2
+            // 
+            this.powerUpTimer2.Enabled = true;
+            this.powerUpTimer2.Interval = 5000;
+            this.powerUpTimer2.Tick += new System.EventHandler(this.powerUpTimer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 581);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.scoreLabel2);
+            this.Controls.Add(this.scoreLabel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Square Chaser";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label scoreLabel1;
+        private System.Windows.Forms.Label scoreLabel2;
+        private System.Windows.Forms.Timer powerUpTimer;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Timer powerUpTimer2;
+    }
+}
+
---
+++ square chaser/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+//Sean Woods, Tuesday May 7th 2024, basic square chaser game
+namespace square_chaser
+{
+    public partial class Form1 : Form
+    {
+        //All shapes
+        Rectangle top = new Rectangle(0, 20, 600, 10);
+        Rectangle bottom = new Rectangle(0, 551, 600, 10);
+        Rectangle left = new Rectangle(0, 0, 10, 600);
+        Rectangle right = new Rectangle(574, 0, 10, 600);
+        Rectangle player1 = new Rectangle(20, 40, 20, 20);
+        Rectangle player2 = new Rectangle(544, 40, 20, 20);
+        Rectangle ball = new Rectangle(300, 310, 10, 10);
+        Rectangle powerUp = new Rectangle(300, 380, 10, 10); 
+
+        int player1Score = 0;
+        int player2Score = 0;
+        int playerSpeed1 = 5;
+        int playerSpeed2 = 5;
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool dPressed = false;
+        bool aPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
+        bool leftPressed = false;
+        bool rightPressed = false;
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+        Random randGen = new Random();
+
+        //Sound players
+        SoundPlayer player = new SoundPlayer(Properties.Resources.airPlaneDing);
+        SoundPlayer player3 = new SoundPlayer(Properties.Resources.win);
+        SoundPlayer player4 = new SoundPlayer(Properties.Resources.hitWall);
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Randomized ball generation
+            ball.X = randGen.Next(30, 532);
+            ball.Y = randGen.Next(10, 554);
+            powerUp.X = randGen.Next(30, 532);
+            powerUp.Y = randGen.Next(10, 554);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Fill colors
+            e.Graphics.FillRectangle(redBrush, top);
+            e.Graphics.FillRectangle(redBrush, bottom);
+            e.Graphics.FillRectangle(redBrush, left);
+            e.Graphics.FillRectangle(redBrush, right);
+            e.Graphics.FillRectangle(greenBrush, player2);
+            e.Graphics.FillRectangle(greenBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, ball);
+            e.Graphics.FillRectangle(yellowBrush, powerUp);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Key controls
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Key controls 
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerMovement();
+
+            //check if player1 hit the square
+            if (player1.IntersectsWith(ball))
+            {
+                player.Play();
+                int randY = randGen.Next(30, 532);
+                int randX = randGen.Next(10, 554);
+                ball.X = randX;
+                ball.Y = randY;
+                player1Score++;
+                scoreLabel1.Text = $"Player 1 Score: {player1Score}";    
+            }
+
+            //check if player2 hit the square
+            if (player2.IntersectsWith(ball))
+            {
+                player.Play();
+                int randY = randGen.Next(30, 532);
+                int randX = randGen.Next(10, 554);
+                ball.X = randX;
+                ball.Y = randY;
+                player2Score++;
+                scoreLabel2.Text = $"Player 2 Score: {player2Score}";           
+            }
+
+            //check if player1 hit the power up
+            if (player1.IntersectsWith(powerUp))
+            {
+                player.Play();
+                int randY = randGen.Next(30, 532);
+                int randX = randGen.Next(10, 554);
+                powerUp.X = randX;
+                powerUp.Y = randY;
+                playerSpeed1 = 10;   
+                powerUpTimer.Start();
+            }
+
+            //check if player2 hit the power up
+            if (player2.IntersectsWith(powerUp))
+            {
+                player.Play();
+                int randY = randGen.Next(30, 532);
+                int randX = randGen.Next(10, 554);
+                powerUp.X = randX;
+                powerUp.Y = randY;
+                playerSpeed2 = 10;
+                powerUpTimer2.Start();
+            }
+
+            //check to see who wins
+            if (player1Score == 5)
+            {
+                player3.Play();
+                winLabel.Text = "Player 1 Wins";
+                gameTimer.Stop();
+            }
+            if (player2Score == 5)
+            {
+                player3.Play();
+                winLabel.Text = "Player 2 Wins";
+                gameTimer.Stop();
+            }
+            Refresh();
+        }
+
+        private void powerUpTimer_Tick(object sender, EventArgs e)
+        {
+            //resetting player1 speed after power up
+            playerSpeed1 = 5;
+        }
+
+        private void powerUpTimer2_Tick(object sender, EventArgs e)
+        {
+            //resetting player2 speed after power up
+            playerSpeed2 = 5;
+        }
+
+        public void PlayerMovement()
+        {
+            //move player 1
+            if (wPressed == true)
+            {
+                player1.Y = player1.Y - playerSpeed1;
+
+                if (player1.IntersectsWith(top))
+                {
+                    player4.Play();
+                    player1.Y = 30;
+                }
+            }
+            if (sPressed == true)
+            {
+                player1.Y = player1.Y + playerSpeed1;
+
+                if (player1.IntersectsWith(bottom))
+                {
+                    player4.Play();
+                    player1.Y = 531;
+                }
+            }
+            if (aPressed == true)
+            {
+                player1.X = player1.X - playerSpeed1;
+
+                if (player1.IntersectsWith(left))
+                {
+                    player4.Play();
+                    player1.X = 10;
+                }
+            }
+            if (dPressed == true)
+            {
+                player1.X = player1.X + playerSpeed1;
+
+                if (player1.IntersectsWith(right))
+                {
+                    player4.Play();
+                    player1.X = 554;
+                }
+            }
+
+            //move player 2
+            if (upPressed == true)
+            {
+                player2.Y = player2.Y - playerSpeed2;
+
+                if (player2.IntersectsWith(top))
+                {
+                    player4.Play();
+                    player2.Y = 30;
+                }
+            }
+            if (downPressed == true)
+            {
+                player2.Y = player2.Y + playerSpeed2;
+
+                if (player2.IntersectsWith(bottom))
+                {
+                    player4.Play();
+                    player2.Y = 531;        
+                }
+            }
+            if (rightPressed == true)
+            {
+                player2.X = player2.X + playerSpeed2;
+
+                if (player2.IntersectsWith(right))
+                {
+                    player4.Play();
+                    player2.X = 554;
+                }
+            }
+            if (leftPressed == true)
+            {
+                player2.X = player2.X - playerSpeed2;
+
+                if (player2.IntersectsWith(left))
+                {
+                    player4.Play();
+                    player2.X = 10;
+                }
+            }
+        }
+    }
+}
